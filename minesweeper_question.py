6b31c605e3a874a650b9a1ba431990c66a940c4b
ShaunSEMO <tshegofatsosemenya@gmail.com>
2021-03-19 04:12:51 +0200

    init commit

---
+++ minesweeper_question.py
@@ -0,0 +1,31 @@
+import random
+import time
+
+print("Welcome to Minesweeper! To play, please enter your name, board dimensions and number of bombs \nRemeber the number of bombs cannot exceed the board dimensions.")
+print("\n")
+time.sleep(2)
+
+name = input("Name: ")
+
+# You may create additional functions here:
+
+
+# Additional Functions above this comment
+
+# Implement your Minesweeper Solution Below:
+
+
+
+def play(dim_size, num_bombs):
+    #Edit the code Below Here
+
+
+
+    pass
+    #Edit the code Above Here
+#play Function Ends Here
+
+
+# if __name__=='__main__':
+#     play()
+    
