f8ae720a6ffc8f8a4226e32b6c82aacbbbe52635
ShaunSEMO <tshegofatsosemenya@gmail.com>
2021-03-19 05:18:38 +0200

    Board print

--- minesweeper_question.py
+++ minesweeper_question.py
@@ -1,27 +1,49 @@
 import random
-import time
 
-print("Welcome to Minesweeper! To play, please enter your name, board dimensions and number of bombs \nRemeber the number of bombs cannot exceed the board dimensions.")
+print("Welcome to Minesweeper! \nTo play, please enter your name, board dimensions and number of bombs \nRemeber the number of bombs cannot exceed the board dimensions.")
 print("\n")
-time.sleep(2)
 
 name = input("Name: ")
+input_board_dim = int(input("Board dimension(Enter number only): "))
+input_bomb_num = int(input("Number of bombs: "))
+print('\n')
 
+y_axis = []
+x_axis = []
+
+top_line = ""
+bottom_line = ""
+
+blocks = ''
 # You may create additional functions here:
 
+for num in range(input_board_dim+1):
+    # blocks += f' {random.randint(num, input_board_dim)} |'
+    blocks += f'   |'
+    top_line += f'___{num}'
+    bottom_line += '----'
+    y_axis.append(num)
+    x_axis.append(num)
 
+def print_board(y,x):
+    print(top_line)
+    for i in y:
+        print(f'{i}|{blocks} ')
+    print(bottom_line)
+
+print_board(y_axis, x_axis)
 # Additional Functions above this comment
 
 # Implement your Minesweeper Solution Below:
 
 
 
-def play(dim_size, num_bombs):
-    #Edit the code Below Here
+# def play(dim_size, num_bombs):
+#     #Edit the code Below Here
 
 
 
-    pass
+#     pass
     #Edit the code Above Here
 #play Function Ends Here
 
